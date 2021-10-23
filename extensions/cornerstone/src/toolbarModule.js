164f77282d4b2f8fb3daaaf35f6c09632f6718f7
maen1 <mk.maen93@gmail.com>
2021-10-23 15:39:50 +0200

    add segmentation dropdown

--- extensions/cornerstone/src/toolbarModule.js
+++ extensions/cornerstone/src/toolbarModule.js
@@ -99,14 +99,50 @@ const definitions = [
     commandName: 'setToolActive',
     commandOptions: { toolName: 'Brush' },
   },
+
+
   {
-    id: 'FreehandScissors',
-    label: 'FreehandScissors',
-    icon: 'star',
-    //
-    type: TOOLBAR_BUTTON_TYPES.SET_TOOL_ACTIVE,
-    commandName: 'setToolActive',
-    commandOptions: { toolName: 'FreehandScissors' },
+    id: 'SegDropdown',
+    label: 'Segmentation',
+    icon: 'ellipse-circle',
+    buttons: [
+      {
+        id: 'Brush',
+        label: 'Brush',
+        icon: 'brush',
+        //
+        type: TOOLBAR_BUTTON_TYPES.SET_TOOL_ACTIVE,
+        commandName: 'setToolActive',
+        commandOptions: { toolName: 'Brush' },
+      },
+      {
+        id: 'SphericalBrush',
+        label: 'Spherical',
+        icon: 'sphere',
+        //
+        type: TOOLBAR_BUTTON_TYPES.SET_TOOL_ACTIVE,
+        commandName: 'setToolActive',
+        commandOptions: { toolName: 'SphericalBrush' },
+      },
+      {
+        id: 'CorrectionScissors',
+        label: 'Correction Scissors',
+        icon: 'scissors',
+        //
+        type: TOOLBAR_BUTTON_TYPES.SET_TOOL_ACTIVE,
+        commandName: 'setToolActive',
+        commandOptions: { toolName: 'CorrectionScissors' },
+      },
+      {
+        id: 'BrushEraser',
+        label: 'Eraser',
+        icon: 'trash',
+        //
+        type: TOOLBAR_BUTTON_TYPES.SET_TOOL_ACTIVE,
+        commandName: 'setToolActive',
+        commandOptions: { toolName: 'BrushEraser' },
+      },
+    ],
   },
 
   {
